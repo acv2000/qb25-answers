05ab9b296525cfa2acf81622d1d0163343faa3a3
acv2000 <averbek1@jhu.edu>
2025-09-04 10:45:38 -0400

    Add files via upload

---
+++ python-commands.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import sys
+
+my_file = open( sys.argv[1] )
+
+for line in my_file: 
+    column = line.strip("\n") #at beginning of every line typical of bed files 
+    column2 = column.split("\t")
+    original_score = int(column2[4])
+    start = int(column2[1])
+    end = int(column2[2])
+    new_score = original_score*(end - start)
+    strand = column2[5]
+    if strand =="-":
+        finalnew_score = new_score * -1
+    else:
+        finalnew_score = new_score
+    print(f"{column2[0]}\t{start}\t{end}\t{column2[3]}\t{finalnew_score}\t{strand}")
