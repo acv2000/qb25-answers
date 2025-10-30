9376100fd4abf32e51940a20b5e0d12d3348d036
Alex V <averbek1@jh.edu>
2025-10-30 15:19:08 -0400

    week7

---
+++ week7/README.md
@@ -0,0 +1,9 @@
+1.3 What does the PCA plot suggest to you about the data? Why?
+The plot suggests that in the data from the third replicate of the LFC-Fe section might have been swapped with the data from the first replicate from the Fe section. Replicates are always expected to cluster together and the accidental swapping of the labels would explain this mismatch perfectly.
+
+PC1 usually captures variation at the technical level as well as biological differences like tissue type or developmental stage. 
+
+3.1 GO Analysis
+In cluster 3 genes involved in innate immunity and proteolysis are enriched. Cluster 3 corresponds primarily to cells in the anterior midgut which is a primary site for innate immunity and proteolysis is a part of this process. 
+
+In cluster 7 genes involved in vitelline membrane formation are enriched, considering this data is from the midgut this is very strange. 
---
+++ week7/week7_assignment.R
@@ -0,0 +1,91 @@
+# Exercise 1: PCA analysis
+setwd("~/qb25-answers/week7")
+
+read_matrix <-read.delim("read_matrix.tsv", row.names = 1, check.names = FALSE)
+read_matrix = as.matrix(read_matrix)
+
+reads_sds <- rowSds(read_matrix)
+top500_vargene <- order(reads_sds, decreasing = TRUE)[1:500]
+expr_top500 <- read_matrix[top500_vargene, ]
+
+#Fix swapped labels
+col <- colnames(expr_top500)
+swap <- match(c("LFC-Fe_Rep3", "Fe_Rep1"), col)
+col[swap] <- col[rev(swap)]
+colnames(expr_top500) <- col
+
+expr_t <- t(expr_top500)
+norm_expr_t = scale(expr_t)
+pca_expr <- prcomp(expr_t, center = TRUE, scale. = TRUE)
+summary(pca_expr)
+
+pca_df <- as_tibble(pca_expr$x[, 1:2], rownames = "Sample")
+pca_df <- pca_df %>%
+  tidyr::separate(Sample, into = c("Tissue", "Replicate"), sep = "_", remove = FALSE)
+
+pca_summary <- tibble(
+  PC = 1:length(pca_expr$sdev),
+  sd = pca_expr$sdev) %>%
+  mutate(
+    var = sd^2,
+    norm_var = var / sum(var))
+
+pca_summary %>% ggplot(aes(PC, norm_var)) + 
+  geom_line() +
+  geom_point()
+ggsave("PCA_variance_plot.png", width = 6, height = 4)
+
+ggplot(pca_df, aes(x = PC1, y = PC2, color = Tissue, shape = Replicate)) +
+  geom_point(size = 3) +
+  labs(title = "PCA of Top 500 Variable Genes",
+       x = "PC1",
+       y = "PC2") +
+  theme_minimal()
+ggsave("PCA_plot.png", width = 6, height = 5)
+
+# Calculate variance
+variance_pca <- (pca_results$sdev)^2 / sum(pca_results$sdev^2)
+
+bar_df <- tibble(PC = paste0("PC", 1:length(variance_pca)),
+                 Variance = variance_pca)
+
+ggplot(bar_df, aes(x = PC, y = Variance)) +
+  geom_bar(stat = "identity", fill = "plum") +
+  theme_minimal() +
+  labs(title = "Variance Explained by Principal Components",
+       x = "Principal Component",
+       y = "Proportion of Variance Explained")
+ggsave("PCA_variance.png", width = 6, height = 4)
+
+# Exercise 2: K-means clustering
+combined <- expr_top500[, seq(1, 21, 3)]
+combined <- combined + expr_top500[, seq(2, 21, 3)]
+combined <- combined + expr_top500[, seq(3, 21, 3)]
+combined <- combined / 3
+
+gene_var_combined <- rowSds(combined)
+combined_fil <- combined[gene_var_combined > 1, ]
+
+set.seed(42)
+k_clusters <- kmeans(combined_fil, centers = 12, nstart = 100)
+labels <- k_clusters$cluster
+
+sorted_id <- order(labels)
+sorted_data <- combined_fil[sorted_id, ]
+sorted_labels <- labels[sorted_id]
+
+library(RColorBrewer)
+png("heatmap.png", width = 800, height = 800)
+heatmap(sorted_data,
+        Rowv = NA, Colv = NA,
+        RowSideColors = brewer.pal(12, "Paired")[sorted_labels],
+        ylab = "Gene")
+dev.off()
+
+# Exercise 3: Gene ontology enrichment analysis
+cluster3_genes <- rownames(sorted_data)[sorted_labels == 3]
+cluster7_genes <- rownames(sorted_data)[sorted_labels == 2]
+
+write.table(cluster3_genes, "cluster3_genes.txt", quote = FALSE, row.names = FALSE, col.names = FALSE)
+write.table(cluster7_genes, "cluster7_genes.txt", quote = FALSE, row.names = FALSE, col.names = FALSE)
+
